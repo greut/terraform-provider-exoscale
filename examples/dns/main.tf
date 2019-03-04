50a82ae1810f8e14d86485a146809deef349b083
Yoan Blanc <yoan.blanc@exoscale.ch>
2019-03-04 14:30:30 +0100

    example: dns 0.12
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- examples/dns/main.tf
+++ examples/dns/main.tf
@@ -1,7 +1,7 @@
 provider "exoscale" {
-  version = "~> 0.9.20"
-  key = "${var.key}"
-  secret = "${var.secret}"
+  version = "~> 0.10.0"
+  key     = var.key
+  secret  = var.secret
 }
 
 resource "exoscale_domain" "exo" {
@@ -9,23 +9,24 @@ resource "exoscale_domain" "exo" {
 }
 
 resource "exoscale_domain_record" "root" {
-  domain = "${exoscale_domain.exo.name}"
-  content = "159.100.200.1"
-  name = ""
+  domain      = exoscale_domain.exo.name
+  content     = "159.100.200.1"
+  name        = ""
   record_type = "A"
 }
 
 resource "exoscale_domain_record" "www" {
-  domain = "${exoscale_domain.exo.name}"
-  content = "${exoscale_domain_record.root.hostname}"
-  name = "www"
+  domain      = exoscale_domain.exo.name
+  content     = exoscale_domain_record.root.hostname
+  name        = "www"
   record_type = "CNAME"
-  ttl = 7200
+  ttl         = 7200
 }
 
 resource "exoscale_domain_record" "hello" {
-  domain = "${exoscale_domain.exo.name}"
-  content = "hello world!"
-  name = ""
+  domain      = exoscale_domain.exo.name
+  content     = "hello world!"
+  name        = ""
   record_type = "TXT"
 }
+
